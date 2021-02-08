cbab28d0467a72dcda8c96e201f5bce72ece51d8
Alexander Gor <ornot.work@gmail.com>
2021-02-08 20:08:15 +0200

    TB-686. Distinct filters fix

--- trood/contrib/django/tests/settings.py
+++ trood/contrib/django/tests/settings.py
@@ -1,4 +1,12 @@
+import os
+
 class MockSettings:
+    LANGUAGE_CODE = 'en-us'
+    ALLOWED_HOSTS = ['*']
+    EMAIL_BACKEND = ''
+    MIGRATION_MODULES = []
+    DEFAULT_INDEX_TABLESPACE = ''
+    CACHES = ''
     REST_FRAMEWORK = {
         'DEFAULT_AUTHENTICATION_CLASSES': [],
         'DEFAULT_PERMISSION_CLASSES': [],
@@ -17,9 +25,10 @@ class MockSettings:
     DATABASE_ROUTERS = []
     DATA_UPLOAD_MAX_MEMORY_SIZE = 2621440
     DATA_UPLOAD_MAX_NUMBER_FIELDS = 1000
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     DATABASES = {'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }}
     ABSOLUTE_URL_OVERRIDES = {}
     SERVICE_DOMAIN = "TEST"
