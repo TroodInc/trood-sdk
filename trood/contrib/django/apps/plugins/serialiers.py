ac29687ff87cdb0d00bf8ce01dfafb4c96353712
Kirill Pastukhov <kirush-ka@yandex.ru>
2021-04-22 16:13:37 +0300

    TB-786. Expose plugin active field

--- trood/contrib/django/apps/plugins/serialiers.py
+++ trood/contrib/django/apps/plugins/serialiers.py
@@ -6,4 +6,4 @@ from trood.contrib.django.apps.plugins.models import TroodPluginModel
 class TroodPluginSerizalizer(serializers.ModelSerializer):
     class Meta:
         model = TroodPluginModel
-        fields = ('id', 'name', 'version', 'config')
+        fields = '__all__'
